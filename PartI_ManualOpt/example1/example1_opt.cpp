127fb7c976cc3a9346a7e201cc23c4cb5732b78a
Braden Helmer <bradenhelmeraus@gmail.com>
2024-04-15 15:05:43 -0400

    Manual example 1 and 2

--- PartI_ManualOpt/example1/example1_opt.cpp
+++ PartI_ManualOpt/example1/example1_opt.cpp
@@ -16,13 +16,12 @@
  * =====================================================================================
  */
 
-
+#include <assert.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <assert.h>
-#include <unistd.h>
 #include <sys/time.h>
+#include <unistd.h>
 
 typedef long LARGE_INTEGER;
 
@@ -32,6 +31,7 @@ double x[m][n];
 double y[n][m];
 double s[m][n];
 double r[m][n];
+double temp[m][n];
 
 #define TIME
 
@@ -41,60 +41,62 @@ double r[m][n];
 #define IF_TIME(foo)
 #endif
 
-//array initialization
-void init_array()
-{
+// array initialization
+void init_array() {
   int i, j;
 
-  for (i=0; i<m; i++) {
-    for (j=0; j<n; j++) {
-        x[i][j] = i + j;
-        y[j][i] = i - j;
-        s[i][j] = i * j;
-        r[i][j] = 0;
-
+  for (i = 0; i < m; i++) {
+    for (j = 0; j < n; j++) {
+      x[i][j] = i + j;
+      y[j][i] = i - j;
+      s[i][j] = i * j;
+      r[i][j] = 0;
     }
   }
 }
 
-
-//define timer for performance measurement
-double rtclock()
-{
+// define timer for performance measurement
+double rtclock() {
   struct timezone Tzp;
   struct timeval Tp;
   int stat;
-  stat = gettimeofday (&Tp, &Tzp);
-  if (stat != 0) printf("Error return from gettimeofday: %d",stat);
-  return(Tp.tv_sec + Tp.tv_usec*1.0e-6);
+  stat = gettimeofday(&Tp, &Tzp);
+  if (stat != 0)
+    printf("Error return from gettimeofday: %d", stat);
+  return (Tp.tv_sec + Tp.tv_usec * 1.0e-6);
 }
 double t_start, t_end;
 
+// example1
+double example1_opt() {
 
-//example1
-double example1(){
-  for (int i=0; i<m; i++){
-    for (int j=0; j < m; j++){
-      for (int k=0; k < n; k++){
-        for (int l=0; l < n; l++){
-          r[i][k] +=  x[i][l] * y[l][j] * s[j][k];
-        }
+  for (int l = 0; l < n; l++) {
+    for (int k = 0; k < n; k++) {
+      for (int j = 0; j < m; j++) {
+        temp[l][k] = y[l][j] * s[j][k];
       }
     }
   }
-  return r[m-1][n-1];
+
+  for (int i = 0; i < m; i++) {
+    for (int k = 0; k < n; k++) {
+      for (int l = 0; l < n; l++) {
+        r[i][k] = x[i][l] * temp[l][k];
+      }
+    }
+  }
+
+  return r[m - 1][n - 1];
 }
 
-int main()
-{
+int main() {
   init_array();
 
   IF_TIME(t_start = rtclock());
-  double rst_org = example1();
+  double rst_org = example1_opt();
   IF_TIME(t_end = rtclock());
   IF_TIME(printf("time %0.6lfs\n", t_end - t_start));
 
   printf("result=%f\n", rst_org);
   return 0;
 }
-
